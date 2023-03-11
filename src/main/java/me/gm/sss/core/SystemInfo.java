f979f70241f4b1a5472df17783dd5fdc862ebfa6
Giacomo Maino <mainogiacomo18@gmail.com>
2023-03-11 19:21:59 +0100

    Basic pregenerated code

---
+++ src/main/java/me/gm/sss/core/SystemInfo.java
@@ -0,0 +1,13 @@
+package me.gm.sss.core;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
